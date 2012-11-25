0b385138fb86835b14378b8ae05a640887b38d9a
Apprentice <apprentice@dbc04.local>
2012-11-24 22:11:53 -0800

    cleaned up seat_geek.rb

--- seat_geek.rb
+++ seat_geek.rb
@@ -5,7 +5,6 @@ require 'open-uri'
 require 'mechanize'
 require 'watir-webdriver'
 require 'selenium/server'
-
 require_relative 'oracle_arena_sections.rb'
 
 class TeamGames
@@ -273,68 +272,4 @@ class Arena
 
 end
 
-#puts OracleArena.new.calculate_seat(@section_hash['14'], 12)
 
-
-
-tickets = Tickets.new('Golden State Warriors')
-
-oracle = Arena.new(tickets, $oracle_arena_hash)
-
-#puts oracle.value_index
-
-#puts tickets.all_available
-
-#puts oracle.get_best_ticket(150)
-
-#puts tickets.all_available.length
-
-#puts tickets.all_available.inspect
-
-
-#
-# ticket_form = page.form('form1')
-# price = ticket_form.field(:name => 'ucTicketList$ddlMinPrice')
-# price.option_with(:value => /500/).select
-# ticket_form.submit
-# #
-# page.search("tr.inventoryRow").each do |node|
-#   puts node
-# end
-
-
-# page = agent.submit(select)
-#pp page
-#pp select
-server = Selenium::Server.new("selenium-server-standalone-2.0b1.jar", :background => true)
-server.start
-
-capabilities = Selenium::WebDriver::Remote::Capabilities.htmlunit(:javascript_enabled => true)
-
-browser = Watir::Browser.new(:remote, :url => "http://127.0.0.1:4444/wd/hub",
-    :desired_capabilities => capabilities)
-
-
-browser.goto'http://www.ticketsnow.com/NBA/InventoryBrowse/Golden-State-Warriors-Tickets-at-ORACLE-Arena-in-Oakland-11-29-2012?PID=1310047'
-#browser.goto 'http://www.ticketsnow.com/NBA/InventoryBrowse/Golden-State-Warriors-Tickets-at-ORACLE-Arena-in-Oakland-11-29-2012?PID=1310047'
-#puts browser.select_list(:name => 'ucTicketList$ddlMinPrice').options
-browser.select_list(:name => 'ucTicketList$ddlMinPrice').select '$100'
-
-browser.element(:css => ".inventoryRow").each do |item|
-  puts item
-end
-
-server.stop
-
-
-
-
-# puts TeamGames.new('Golden State Warriors').home_games[0].opponent
-# puts TeamGames.new('Golden State Warriors').home_games[0].relative_price
-
-
-#puts TeamGames.new('Golden State Warriors').home_price_standard_deviation
-
-#puts TeamGames.new('Los Angeles Lakers').home_games[0].inspect
-
-#puts TeamGames.new('Los Angeles Lakers').home_games
