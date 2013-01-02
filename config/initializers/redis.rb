0eca105f17fee6ab2593ee5c020aeffbc8f73536
Jake Sendar <jakesendar@gmail.com>
2013-01-02 14:44:30 -0500

    adds rediscloud

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,10 +1,2 @@
-require "redis"
-redis_conf_path = Rails.root.join("config/redis", "#{Rails.env}.conf")
-redis_conf = File.read(redis_conf_path)
-port = /port.(\d+)/.match(redis_conf)[1]
-`redis-server #{redis_conf_path}`
-res = `ps aux | grep redis-server`
-unless res.include?("redis-server") && res.include?(redis_conf_path.to_s)  
-  raise "Couldn't start redis"
-  end
-$redis = Redis.new(:port => port)
+uri = URI.parse(ENV["REDISCLOUD_URL"])
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
