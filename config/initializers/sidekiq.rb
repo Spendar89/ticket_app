7aec4702919180361d10cfd0d12adbcb4a74246b
Jake Sendar <jakesendar@gmail.com>
2013-01-03 22:57:51 -0500

    dsfg

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://rediscloud:V3m9nbH6BL6c5qwX@redis-15726.us-east-1-4.1.ec2.garantiadata.com:15726', :namespace => 'mynamespace' }
+  config.redis = { :url => 'redis://rediscloud:V3m9nbH6BL6c5qwX@redis-15726.us-east-1-4.1.ec2.garantiadata.com:15726', :namespace => 'tickets' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :url => 'redis://rediscloud:V3m9nbH6BL6c5qwX@redis-15726.us-east-1-4.1.ec2.garantiadata.com:15726', :namespace => 'tickets' }
 end
