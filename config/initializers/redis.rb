6a5b657687e6ffa89e853cd2ab2275bd40fe8d5e
Jake Sendar <jakesendar@gmail.com>
2013-01-04 14:12:30 -0500

    changes redis config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,14 @@
-uri = URI.parse(ENV["REDISCLOUD_URL"])
-$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+require "redis"
+
+if ::Rails.env == "production"
+  uri = URI.parse(ENV["REDISCLOUD_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  rx = /port.(\d+)/
+  s = File.read("#{::Rails.root}/config/redis/#{::Rails.env}.conf")
+  port = rx.match(s)[1]
+  `redis-server #{::Rails.root}/config/redis/#{::Rails.env}.conf`
+  res = `ps aux | grep redis-server`
+  raise "Couldn't start redis" unless res.include?("redis-server") && res.include?("#{::Rails.root}/config/redis/#{::Rails.env}.conf")
+  $redis = Redis.new(:port => port)
+end
